8d89bbbdd7eb7ad38614d4c5ec9d04092d594986
scott <scottarthurgall@gmail.com>
2019-08-26 18:08:40 -0500

    movies populate and movie details pages work

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <h1><router-link to="/">{{ title }}</router-link></h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    title: String,
+  },
+  data() {
+    return {
+      name: 'Scott'
+    };
+  },
+};
+</script>
+
+<style scoped>
+header {
+  background-color: #111;
+  padding: 20px;
+  color: white;
+}
+h1 {
+  margin: 0;
+}
+</style>
---
+++ src/components/MoviesList.vue
@@ -0,0 +1,49 @@
+<template>
+  <ul>
+    <li v-for="movie in movies">
+      <Movie :movie="movie" />
+    </li>
+  </ul>
+</template>
+
+<script>
+import Movie from './Movie.vue';
+export default {
+  name: 'MoviesList',
+  data() {
+    return {
+      movies: []
+    }
+  },
+  created: function() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData: async function() {
+      try {
+        const res = await fetch(
+          'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=03217c780beb981d10b0d7ef19d8ab4b'
+        );
+        const movies = await res.json();
+        this.movies = movies.results;
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  },
+  components: {
+    Movie
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  display: flex;
+  flex-wrap: wrap;
+  list-style: none;
+}
+li {
+  padding: 1rem;
+}
+</style>
